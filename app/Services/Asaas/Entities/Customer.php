8f1f93bf97de3112ea25c744c7818b8f355b1f6e
Jeff <jeffersonsevero08@gmail.com>
2023-09-05 11:12:43 -0300

    feat: entity de customer

--- app/Services/Asaas/Entities/Customer.php
+++ app/Services/Asaas/Entities/Customer.php
@@ -1,18 +1,20 @@
 <?php
+
 namespace App\Services\Asaas\Entities;
 
 class Customer
 {
-	public readonly string $id;
-	public readonly string $name;
-	public readonly string $cpfCnpj;
+    public string $id;
 
+    public string $name;
 
-	public function __construct(array $data)
-	{
-		$this->id = data_get($data, 'id');
-		$this->name = data_get($data, 'name');
-		$this->cpfCnpj = data_get($data, 'cpfCnpj');
-	}
+    public string $cpfCnpj;
+
+    public function __construct($data)
+    {
+        $this->id      = data_get($data, 'id');
+        $this->name    = data_get($data, 'name');
+        $this->cpfCnpj = data_get($data, 'cpfCnpj');
+    }
 
 }
