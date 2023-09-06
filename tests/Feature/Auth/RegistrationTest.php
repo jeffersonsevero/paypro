36de1e37227d9e42a58cf4d8ffb75cede5dfb4e7
Jeff <jeffersonsevero08@gmail.com>
2023-09-06 15:59:33 -0300

    test: inclusao de evento no registro de usuario

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Providers\RouteServiceProvider;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\Event;
 
 test('registration screen can be rendered', function () {
     $response = $this->get('/register');
@@ -9,12 +11,17 @@ test('registration screen can be rendered', function () {
 });
 
 test('new users can register', function () {
+
+    Event::fake();
+
     $response = $this->post('/register', [
         'name'     => 'Test User',
         'email'    => 'test@example.com',
         'cpf'      => '000.000.000-00',
         'password' => 'password',
     ]);
+
+    Event::assertDispatched(Registered::class);
 
     $this->assertAuthenticated();
     $response->assertRedirect(RouteServiceProvider::HOME);
