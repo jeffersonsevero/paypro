80db8817ce1713e6dd38fa6e9963701c26a7a777
Jeff <jeffersonsevero08@gmail.com>
2023-09-06 15:56:56 -0300

    feat: entity de retorno do pix

---
+++ app/Services/Asaas/Entities/PixQrCode.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Services\Asaas\Entities;
+
+class PixQrCode
+{
+    public ?bool $success;
+
+    public ?string $encodedImage;
+
+    public ?string $payload;
+    public ?string $expirationDate;
+
+    public function __construct(mixed $data)
+    {
+        $this->success    = data_get($data, 'success');
+        $this->encodedImage = data_get($data, 'encodedImage');
+        $this->payload     = data_get($data, 'payload');
+        $this->expirationDate       = data_get($data, 'expirationDate');
+    }
+
+}
