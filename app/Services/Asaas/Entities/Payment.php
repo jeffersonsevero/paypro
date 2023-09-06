b55ee2351a24498ee210d2ed3c458be2d7959eaa
Jeff <jeffersonsevero08@gmail.com>
2023-09-06 16:00:35 -0300

    feat: entity de pagamento

---
+++ app/Services/Asaas/Entities/Payment.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Services\Asaas\Entities;
+
+class Payment
+{
+    public ?string $customer;
+    public ?string $id;
+
+    public ?string $billingType;
+    public ?string $errors;
+
+    public ?string $dueDate;
+    public ?string $bankSlipUrl;
+    public ?string $status;
+
+    public ?int $value;
+
+    public function __construct(mixed $data)
+    {
+        $this->customer    = data_get($data, 'customer');
+        $this->billingType = data_get($data, 'billingType');
+        $this->dueDate     = data_get($data, 'dueDate');
+        $this->value       = data_get($data, 'value');
+        $this->bankSlipUrl = data_get($data, 'bankSlipUrl');
+        $this->status = data_get($data, 'status');
+        $this->id = data_get($data, 'id');
+        $this->errors = data_get($data, 'errors');
+    }
+
+}
