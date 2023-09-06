8f89ca2f0530313ecb6fc0a03a2d70db16898203
Jeff <jeffersonsevero08@gmail.com>
2023-09-06 15:58:26 -0300

    chore: inclusao da trait de pagamentos no service principal

--- app/Services/Asaas/AsaasService.php
+++ app/Services/Asaas/AsaasService.php
@@ -3,12 +3,14 @@
 namespace App\Services\Asaas;
 
 use App\Services\Asaas\Endpoints\HasCustomers;
+use App\Services\Asaas\Endpoints\HasPayments;
 use Illuminate\Http\Client\PendingRequest;
 use Illuminate\Support\Facades\Http;
 
 class AsaasService
 {
     use HasCustomers;
+	use HasPayments;
 
     public PendingRequest $api;
 
