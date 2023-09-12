6c2b6797d0c4a7b5cb6ba96444672b47b501d11b
Jeff <jeffersonsevero08@gmail.com>
2023-09-12 11:59:38 -0300

    feat: metodo para criacao de pagamento via cartao

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Actions\CreatePaymentWithBilletAction;
+use App\Actions\CreatePaymentWithCardAction;
 use App\Actions\CreatePaymentWithPixAction;
+use App\Exceptions\ErrorOnPaymentException;
 use App\Http\Requests\CreatePaymentRequest;
+use App\Http\Requests\HandlePaymentWithCardRequest;
+use App\Services\Asaas\Entities\Payment;
+use Exception;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 
@@ -12,6 +17,7 @@ class PaymentController extends Controller
 {
     public function handle(CreatePaymentRequest $request): RedirectResponse
     {
+
         if($request->get('payment-type') === 'billet') {
 
             $payment = (new CreatePaymentWithBilletAction($request->get('price')))->handle();
@@ -22,6 +28,43 @@ class PaymentController extends Controller
             $payment = (new CreatePaymentWithPixAction($request->get('price')))->handle();
 
             return redirect()->to(route('payment.success', ['payment' => $payment]));
+        } elseif($request->get('payment-type') === 'card') {
+            return redirect()->to(route('payment.checkout', ['value' => $request->get('price')]));
+        }
+    }
+
+    public function checkout(): View
+    {
+        $data = array_merge(request()->all(), config('checkout'));
+
+        return view('payments.checkout', $data);
+    }
+
+    public function paymentWithCreditCard(HandlePaymentWithCardRequest $request)
+    {
+        try {
+
+            $name = $request->get('name');
+            $serielize = collect($request->all())->except('name')->map(function ($data) {
+                return toOnlyDigits($data);
+            });
+
+            $serielize->prepend($name, 'name');
+
+            /** @var Payment */
+            $payment = (new CreatePaymentWithCardAction($serielize->toArray()))->handle();
+
+            if($payment->id) {
+                toastr()->success('Cobrança efetuada com sucesso!');
+                return redirect()->to(route('dashboard'));
+            }
+
+        } catch(ErrorOnPaymentException $exception) {
+            toastr()->error($exception->getMessage());
+            return redirect()->back();
+        } catch(Exception $exception) {
+            toastr()->error('Ooops, ocorreu algum erro interno');
+            logger()->emergency($exception->getMessage());
         }
     }
 
