499f2217fa7585a612ba2bcab4617e03ab0cfa3c
Jeff <jeffersonsevero08@gmail.com>
2023-09-12 11:59:05 -0300

    feat: tratamento de erro ao criar customer

--- app/Services/Asaas/Endpoints/Customers.php
+++ app/Services/Asaas/Endpoints/Customers.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\Asaas\Endpoints;
 
+use App\Exceptions\ErrorOnCreateCustomerException;
 use App\Services\Asaas\Entities\Customer;
 use App\Services\Asaas\Requests\{CreateCustomerDTO, CreateCustomerRequest};
 
@@ -9,7 +10,13 @@ class Customers extends BaseEndpoint
 {
     public function post(CreateCustomerDTO $data): Customer
     {
+
         $json = $this->service->api->post('/customers', $data->toArray())->json();
+
+        if(isset($json['errors'])) {
+            throw new ErrorOnCreateCustomerException($json['errors'][0]['description'], 400);
+
+        }
 
         return $this->transform(
             $json,
